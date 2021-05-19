08869dc2cbd8c1904acbb83c9768d8ece71a2958
HIMANSHU MANOHAR PATIL <74021207+patil95himanshu@users.noreply.github.com>
2021-05-20 01:08:57 +0530

    Prime1toN

--- Basic/Prime1toN.java
+++ Basic/Prime1toN.java
@@ -3,16 +3,17 @@ public class Prime1toN
 {
 	public static void main(String[] args) 
 	{
+		int a,x,i,j;
 		Scanner sc=new Scanner(System.in);
 		System.out.print("Please enter a number : ");
-		int a=sc.nextInt();
+		a=sc.nextInt();
 		
-		int x;
+		
 		System.out.print("Prime numbers from 1 to "+a+" : ");
-		for(int j=2;j<=a;j++)
+		for(j=2;j<=a;j++)
 		{
 			x=0;
-			for(int i=2;i<=j;i++)
+			for(i=2;i<=j;i++)
 				if(j%i==0)
 						x++;
 			if(x==1)
