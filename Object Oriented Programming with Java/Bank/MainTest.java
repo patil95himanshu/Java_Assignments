b8393aa4791271d1649d76e5603e2aa1826701af
HIMANSHU PATIL <74021207+patil95himanshu@users.noreply.github.com>
2021-06-08 00:57:23 +0530

    Build a class which has references to other classes. Instantiate these reference variables and invoke instance methods.

---
+++ Object Oriented Programming with Java/Bank/MainTest.java
@@ -0,0 +1,11 @@
+public class MainTest
+{
+	
+	public static void main(String[] args)
+	{
+      Customer c2 = new Customer("Himanshu",9158781153L,"12-11-1995",8329932253L);
+      Bank b = new Bank("BOI",647,"Thane",c2);
+      System.out.println(b);
+	}
+}
+    
